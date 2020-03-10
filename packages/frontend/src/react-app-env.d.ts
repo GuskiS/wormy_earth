52ab61291ec628049d9c31ee0696a187263380ec
Mārcis <shweede@gmail.com>
2020-03-10 22:26:45 +0200

    Add types for perlin

--- packages/frontend/src/react-app-env.d.ts
+++ packages/frontend/src/react-app-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="react-scripts" />
 
-declare module "@mohayonao/perlin-noise"
+declare module "@mohayonao/perlin-noise" {
+  class Perlin {
+    constructor(randomizer?: () => number) {}
+
+    public noise = (value: number): number => {};
+  }
+
+  export default Perlin;
+}
