01bbe2e015b6b87da18c7a0a43e4b4cbaa5aca48
Krista Titova <krista.kazina@gmail.com>
2020-03-10 22:26:30 +0200

    Declare Perlin

--- packages/frontend/src/react-app-env.d.ts
+++ packages/frontend/src/react-app-env.d.ts
@@ -1,3 +1,10 @@
 /// <reference types="react-scripts" />
 
-declare module "@mohayonao/perlin-noise";
+declare module "@mohayonao/perlin-noise" {
+  class Perlin {
+    constructor(randomizer?: () => number) {}
+
+    public noise = (value: number): number => {};
+  }
+  export default Perlin
+}
